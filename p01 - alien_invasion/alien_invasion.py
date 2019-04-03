b3b58001d304bed47938634385587e6835b5d6e5
Ian Paul Daniels <ianpauldaniels@gmail.com>
2019-04-03 07:19:31 -0400

    Ship Movement update

--- p01 - alien_invasion/alien_invasion.py
+++ p01 - alien_invasion/alien_invasion.py
@@ -3,7 +3,6 @@ import pygame
 from settings import Settings
 from ship import Ship
 import game_functions as gf
-~~
 
 def run_game():
     # Initiate pygame, settings and create a screen object
@@ -17,11 +16,12 @@ def run_game():
     ai_settings.bg_color = (230, 230, 230)
 
     # Make a ship
-    ship = Ship(screen)
+    ship = Ship(ai_settings,screen)
 
     # Start the main loop for the game
     while True:
-        gf.check_events()
+        gf.check_events(ship)
+        ship.update()
         gf.update_screen(ai_settings, screen, ship)
 
 
