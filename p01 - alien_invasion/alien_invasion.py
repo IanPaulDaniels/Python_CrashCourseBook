d2e28a7cb351dd108f8439e303c827bec97310d7
Ian Paul Daniels <ianpauldaniels@gmail.com>
2019-05-03 22:39:35 -0400

    Bullet Update

--- p01 - alien_invasion/alien_invasion.py
+++ p01 - alien_invasion/alien_invasion.py
@@ -1,8 +1,10 @@
 import pygame
+from pygame.sprite import Group
 
 from settings import Settings
 from ship import Ship
 import game_functions as gf
+
 
 def run_game():
     # Initiate pygame, settings and create a screen object
@@ -16,13 +18,16 @@ def run_game():
     ai_settings.bg_color = (230, 230, 230)
 
     # Make a ship
-    ship = Ship(ai_settings,screen)
+    ship = Ship(ai_settings, screen)
+    # Make a group to store bullets in.
+    bullets = Group()
 
     # Start the main loop for the game
     while True:
-        gf.check_events(ship)
+        gf.check_events(ai_settings, screen, ship, bullets)
         ship.update()
-        gf.update_screen(ai_settings, screen, ship)
+        bullets.update()
+        gf.update_screen(ai_settings, screen, ship, bullets)
 
 
 run_game()
